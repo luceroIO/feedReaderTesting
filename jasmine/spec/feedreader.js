8c6d1afe8c54d01335b94a0f8c36b2c20ba6e64f
lucerio <lucero.develops@gmail.com>
2017-08-20 15:12:56 -0400

    setup a test that the menu element is hidden by default

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,35 +31,40 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it('URL data is defined', function(){
+        it('url data is defined', function(){
             allFeeds.forEach(function(feed){
                 expect(feed.url).toBeTruthy();
                 expect(feed.url.length).toBeGreaterThan(0);
             });
-         });
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-         it('Name data is defined', function(){
+        it('name data is defined', function(){
             allFeeds.forEach(function(feed){
                 expect(feed.url).toBeTruthy();
                 expect(feed.url.length).toBeGreaterThan(0);
             });
-         });
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function(){
-        
+
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is hidden by default', function(){
+            console.log($('body').hasClass());
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
